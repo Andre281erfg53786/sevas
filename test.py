d6d52641c843b8deb607444b6ac579032ac6a3ad
Your Name <you@example.com>
2025-05-17 17:39:51 +0300

    Add existing file

--- test.py
+++ test.py
@@ -6,11 +6,33 @@ BOT_TOKEN = '7740470916:AAEqWao7o1IdaL-dtnta99HS8gj6KR6nmVI'
 starter = fsm.FSM()
 bot = telebot.TeleBot(BOT_TOKEN)
 
+def return_to_menu (chat_id):
+        starter.set_state(chat_id , fsm.DEFULT_STATE)
+        bot.send_message(chat_id, text='Ввидете описание фото.' , reply_markup=keyboards.MainKeyboard)
 
+def hadel_default_state(message)  :
+        if message.text == 'Фото' :
+            starter.set_state(message.chat.id , fsm.IMAGE_STATE)
+            bot.send_message(message.chat.id, text='Ввидете описание фото.' , reply_markup=keyboards.BackKeyboard)
+        elif message.text == 'Текст' :
+            starter.set_state(message.chat.id , fsm.TEXT_STATE)
+            bot.send_message(message.chat.id, text='Общайся со мной' , reply_markup=keyboards.BackKeyboard)
+        else:
+            return_to_menu(message.chat.id)
 
+def hadel_image_state (message):
+        if message.text == 'Назад' :
+            return_to_menu(message.chat.id)
+        else:
+            bot.send_message(message.chat.id, text='Скоро будет генирация фото...' )
 
+def hadel_text_state (message):
+        if message.text == 'Назад' :
+            return_to_menu(message.chat.id)
+        else:
+            bot.send_message(message.chat.id, text='Скоро будет генирация ъэ...' )
 
-
+        
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     msg_text = message.text
@@ -24,41 +46,6 @@ def echo_all(message):
         hadel_text_state(message)
     else:
         return_to_menu(message.chat.id)
-
-
-    def return_to_menu (chat.id):
-        starter.set_state(message.chat.id , fsm.DEFULT_STATE)
-        bot.send_message(message.chat.id, text='Ввидете описание фото.' , reply_markup=keyboards.MainKeyboard)
-
-
-
-    def hadel_default_state(message)  :
-        if msg_text == 'Фото' :
-            starter.set_state(message.chat.id , fsm.IMAGE_STATE)
-            bot.send_message(message.chat.id, text='Ввидете описание фото.' , reply_markup=keyboards.BackKeyboard)
-        elif msg_text == 'Текст' :
-            starter.set_state(message.chat.id , fsm.TEXT_STATE)
-            bot.send_message(message.chat.id, text='Общайся со мной' , reply_markup=keyboards.BackKeyboard)
-        else:
-            return_to_menu(message.chat.id)
-
-    def hadel_image_state (message):
-        if msg_text == 'Назад' :
-            return_to_menu(message.chat.id)
-        else:
-            bot.send_message(message.chat.id, text='Скоро будет генирация фото...' )
-
-    def hadel_text_state (message):
-        if msg_text == 'Назад' :
-            return_to_menu(message.chat.id)
-        else:
-            bot.send_message(message.chat.id, text='Скоро будет генирация ъэ...' )
-
-
-
-
-
-
 
             
 bot.polling()
