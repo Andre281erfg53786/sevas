8ab25df7e8bffba14a0e5897ad480eabb27f392c
Your Name <you@example.com>
2025-05-24 17:28:52 +0300

    add skeleton bot

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import telebot 
 import keyboards
 import fsm
+import ai
 
 BOT_TOKEN = '7740470916:AAEqWao7o1IdaL-dtnta99HS8gj6KR6nmVI' 
 starter = fsm.FSM()
+ai_service = ai.AI()
 bot = telebot.TeleBot(BOT_TOKEN)
 
 def return_to_menu (chat_id):
@@ -24,13 +26,22 @@ def hadel_image_state (message):
         if message.text == 'Назад' :
             return_to_menu(message.chat.id)
         else:
-            bot.send_message(message.chat.id, text='Скоро будет генирация фото...' )
+            try:
+                 msg = bot.send_message(chat_id=message.chat.id , text = 'Генерируем...')
+                 image_url = ai_service .generate_image(message.text)
+                 bot.delete_message(chat_id=message.chat.id, message_id=msg.id )
+                 bot.send_photo(chat_id=message.chat.id, caption='Ваше фото' , photo=image_url)
+            except Exception as e:
+                 bot.send_message(chat_id=message.chat.id, text=f'Произоша ошибка ({str(e)})')
 
 def hadel_text_state (message):
         if message.text == 'Назад' :
+            ai_service.clear_dialog(message.chat.id)
             return_to_menu(message.chat.id)
         else:
-            bot.send_message(message.chat.id, text='Скоро будет генирация ъэ...' )
+            msg = bot.send_message(message.chat.id ,  'Думаю над запросом...')
+            txt = ai_service.generate_text(message.text , message.chat.id )
+            msg = bot.edit_message_text(text=txt , chat_id=message.chat.id , message_id=msg.id )
 
         
 @bot.message_handler(func=lambda message: True)
